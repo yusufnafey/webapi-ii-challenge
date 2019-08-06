0b26941217fa5dbf80539b8165c9eca916814dd5
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-06 15:58:11 -0700

    post request complete

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const db = require("./data/db.js");
 // const router = require("./index.js");
 
 const server = express();
 server.use(express.json());
 // server.use("/api/posts", router);
+
+server.post("/", (req, res) => {
+  const user = req.body;
+  if (!user.title || !user.contents) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    db.insert(user)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database."
+        });
+      });
+  }
+});
 
 const port = 8000;
 server.listen(port, () => console.log(`API running in port ${port}`));
